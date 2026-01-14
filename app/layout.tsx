404a53226d782d76f1584ef17dfd1b8f727d9858
ourlovepodcast <ourlovepodcast@gmail.com>
2026-01-14 17:45:47 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 // app/layout.tsx
+import './globals.css'; // This line activates the styles
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
