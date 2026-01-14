6872684e98ce5ba50b298d75318fc3a36e073925
ourlovepodcast <ourlovepodcast@gmail.com>
2026-01-14 18:02:32 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
