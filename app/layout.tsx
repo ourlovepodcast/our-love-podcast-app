8ef7bb703f882519ff2f732b48ff18b725f7f2ca
ourlovepodcast <ourlovepodcast@gmail.com>
2026-01-20 12:55:57 +0100

    Update layout.tsx
    download button

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        {/* TrustBox script loaded via Next.js Script component */}
         <Script 
           src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" 
           strategy="afterInteractive" 
         />
       </head>
-      <body className="antialiased">
+      <body className="antialiased bg-[#030303]">
         {children}
       </body>
     </html>
